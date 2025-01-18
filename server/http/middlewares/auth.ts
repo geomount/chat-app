64a44fe7f3ab7200d51694ed8e22949567550859
geomount <dhrutijid@gmail.com>
2025-01-18 04:13:51 +0000

    Add auth middleware

---
+++ server/http/middlewares/auth.ts
@@ -0,0 +1,26 @@
+import cookieParser from 'cookie-parser';
+import { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import path from "path";
+
+function Auth (req: Request, res: Response, next: NextFunction) {
+    const token = req.cookies.token;
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET as Secret) as JwtPayload;
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+
+        }
+    } else {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+}   
+
+
+export {Auth}
