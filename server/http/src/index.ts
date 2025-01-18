b48076a0c6c49d5904a194f482a8d3d56826f309
geomount <dhrutijid@gmail.com>
2025-01-18 04:15:34 +0000

    Update config files

--- server/http/src/index.ts
+++ server/http/src/index.ts
@@ -1,6 +1,17 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import json, {JwtPayload} from 'jsonwebtoken';
+
 const app = express();
 const PORT = 3000;
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}));
 
 app.get("/", async (req, res) => {
     const username = req.body.username;
