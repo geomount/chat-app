75cc0bdd596f9ec821aec401d17bccd88f2cca26
geomount <dhrutijid@gmail.com>
2025-01-20 18:19:08 +0000

    Add Routes

--- server/http/src/index.ts
+++ server/http/src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import json, {JwtPayload} from 'jsonwebtoken';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import dotenv from 'dotenv';
+import path from "path";
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
 
 app.use(cookieParser());
 app.use(express.json());
@@ -13,12 +18,10 @@ app.use(cors({
     origin: "http://localhost:3000"
 }));
 
-app.get("/", async (req, res) => {
-    const username = req.body.username;
 
-    res.send({
-        message: "Hi from the HTTP Server"
-    })
+app.get("/", (req, res) => {
+    
+    res.send("Hello from the HTTP Server")
 })
 
 app.post("/signup", (req, res) => {
@@ -42,11 +45,21 @@ app.post("/signin", (req, res) => {
 
     // make DB call to check if username and passwords are valid 
     // set cookies here
+    // fetch id from db
+    const token = jwt.sign({
+        id: 1
+    }, JWT_SECRET);
+    res.cookie("token", token);
 
     res.send({
         message: "SignIn Successful! Welcome User"
     })
 })
+
+// app.get("/", async (req, res) => {
+//     res.sendFile(path.join(__dirname, "../../src/index.html"))
+
+// })
 
 app.listen(PORT, () => {
     `HTTP server on PORT: ${PORT}`
