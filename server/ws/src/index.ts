0370b432fbba5c83906e4dd622ad7747558dd054
geomount <dhrutijid@gmail.com>
2025-01-31 18:24:03 +0000

    Added initial WS Backend

--- server/ws/src/index.ts
+++ server/ws/src/index.ts
@@ -1,61 +1,109 @@
-import WebSocket, { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import {JWT_SECRET} from process.env.JWT_SECRET;
 
 const wss = new WebSocketServer({ port: 8080 });
 
-interface User {
-  socket: WebSocket;
-  room: string;
-  val: string | null
+interface UserI {
+  ws?: WebSocket,
+  rooms: string[],
+  userId: string
 }
 
-let allSockets: User[] = [];
+// Inefficent logic:
+// ToDo: Implement Queuing 
 
-interface Response {
-  message: string;
-  val: string
-}
+const users: UserI[] = [];
 
-wss.on('connection', function (socket) {
-  socket.on('error', console.error);
+function checkUser(token: string): string | null {
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (typeof decoded === "string") {
+      return null 
+    }
   
-  socket.on("message", (message: any) => {
-    const parsedMessage = JSON.parse(message.toString());
-    console.log(parsedMessage);
+    if (!decoded || !(decoded.userId)) {
+      return null 
+    }
+  
+    return decoded.userId;
 
-    if (parsedMessage.type === "join"){
-      console.log("user joined room " + parsedMessage.payload.roomId);
+
+  } catch(e) {
+    console.error(e);
+    return null 
+  }
+
+}
+
+wss.on('connection', function connection(ws, request) {
+
+  const url = request.url;
+
+  if (!url) return 
+
+  const queryParameters = new URLSearchParams(url.split('?')[1]);
+  const token = queryParameters.get('token') ?? "";
+  const userId = checkUser(token);
+
+  if (!userId || userId === null){
+    ws.close();
+    return null
+  }
+
+  users.push({
+    userId,
+    rooms: [],
+    ws
+    })
+
+
+  ws.on('message', function message(data) {
     
-    allSockets.push({
-      socket, 
-      room: parsedMessage.payload.roomId,
-      val: null
-    })
+    const parsedData = JSON.parse(data as unknown as string);
+
+    if (parsedData.type === "join_room"){
+      const user = users.find(x => x.ws === ws)
+      const checkRoom = user?.rooms.filter(x => x === parsedData.roomId) || [];
+      console.log(checkRoom);
+      if (checkRoom?.length > 0){ 
+        ws.send("Already in the room")
+        return 
+      }
+      user?.rooms.push(parsedData.roomId)
+      ws.send("Welcome to the room")
+      return
     }
 
-    if (parsedMessage.type === "chat"){
-      console.log("users want to chat");
-
-      const currentUser = allSockets.find(user => user.socket === socket);
-      let currentUserRoom = null;
-
-      if (currentUser) currentUserRoom = currentUser.room;
-
-      allSockets.forEach((user) => {
-        if (user.room === currentUserRoom){
-          if (user.socket === socket){
-            user.val = "S";
-          } else {
-            user.val = "R"
-          }
-          const obj = {
-            message: parsedMessage.payload.message,
-            val: user.val
-          };
-          user.socket.send(JSON.stringify(obj))
-        } 
-    })
+    if (parsedData.type === "leave_room") {
+      const user = users.find(x => x.ws === ws)
+      if (!user) {
+        return 
+      }
+      user.rooms = user?.rooms.filter(x => x === parsedData.roomId);
     }
 
-  })
+    if (parsedData.type === "chat") {
+      const roomId = parsedData.roomId; 
+      const message = parsedData.message;
 
+      users.forEach(user => {
+        if (user.rooms.includes(roomId)) {
+          user.ws?.send(JSON.stringify({
+            type: "chat",
+            message: message, 
+            roomId: roomId
+          }))
+        }
+      }
+      )
+
+    }
+
+
+  });
+
+  ws.send('something');
 });
